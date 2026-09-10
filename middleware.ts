71b051282ef0223f826087f844af617a3625b56f
Denis Lapianso <denislapianso@gmail.com>
2026-09-11 05:53:15 +0700

    fix(middleware): immediately pass /api routes without auth interception

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { updateSession } from '@/utils/supabase/middleware'
-import { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/api')) {
+    return NextResponse.next()
+  }
   return await updateSession(request)
 }
 
